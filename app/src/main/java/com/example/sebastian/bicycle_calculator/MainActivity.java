5eda07ced8b931872ea8ccc0fd5fe6ca8f3f55fb
ssaleta <sebastiansaleta@gmail.com>
2016-05-18 19:27:12 +0200

    <delete-fragments-add-speed>

--- app/src/main/java/com/example/sebastian/bicycle_calculator/MainActivity.java
+++ app/src/main/java/com/example/sebastian/bicycle_calculator/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.sebastian.bicycle_calculator;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -41,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.calculate_btn)
     Button calculateBtn;
 
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +54,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
         ButterKnife.bind(this);
         calculateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.information) {
-            createDialogInformation();
+           /* createDialogInformation();*/
         }
         return super.onOptionsItemSelected(item);
     }
